d1cae378d3d78dbe8133c49ac769a8c1285f9c7a
Tehreem <t1n2b3y4a5h6@gmail.com>
2025-12-19 20:51:45 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,33 @@ const page = () => {
   return (
     <div>
       <figure className='md:flex justify-center items-center bg-slate-500 rounded-xl p-8 md:p-1 dark:bg-slate-800'>      
-        <header className="text-center text-5xl"> Hello world!</header>
+        <header className="text-center text-5xl"> Hey Buddies! 😀</header>
       </figure>
       <figure className='md:flex justify-between items-center bg-blue-100 rounded-xl p-8 md:p-1 dark:bg-blue-800'>
-        <img className="w-24 h-24 rounded-xl" src="/eagle.png" alt="Vibe 2 flex"/>
+        <img className="w-24 h-24 rounded-xl" src="/oggy.jpeg" alt="Vibe 2 flex"/>
         <blockquote>
-          <p className="text-center justify-left text-3xl"> Here is Next.js Project tuned with Tailwind CSS.
-            An effort to learn the coding and to implement it in own design.</p>
+          <p className="text-center justify-left text-3xl"> Hey I am oggy^_^. I was used to entertain people at a time but know that kids who have used to watch me are grown up.
+            Here I just came to say Hello 😊 to  all of you.
+            This project is designed by Tehreem Ashraf.</p>
         </blockquote>
-        <img className="w-30 h-24 rounded-xl" src="/waqas.jpg" alt="Vibe 2 flex"/>
+        <img className="w-30 h-24 rounded-xl" src="/tehreem.jpeg" alt="Vibe 2 flex"/>
       </figure>
       <figure className='md:flex justify-center items-center bg-slate-500 rounded-xl p-8 md:p-1 dark:bg-slate-800'>
       </figure>
       <figure className='md:flex flex-col justify-start bg-green-100 rounded-xl p-8 md:p-1 dark:bg-green-800'>
         <header className="text-left text-4xl"> Message: </header>
         <blockquote>
-          <p className="text-left ml-38 text-2xl"> Keep Learning, Keep Growing. The journey of coding is endless and full of opportunities.
-            Everyday is a new chance to create something amazing. Embrace the challenges, celebrate the victories, and never stop coding!
-            coding is not just a skill, it's a superpower that can change the world. This is just the beginning of an exciting adventure.
-            The deep you dive into coding, the more you realize its potential to transform ideas into reality. 
-            So keep pushing your limits, stay curious, and let your passion for coding lead the way.
+          <p className="text-left ml-38 text-2xl"> Just enjoy your life. You have only one, don't waste it, you must be precious to yourself no matter what others think.
           </p>
         </blockquote>
         <blockquote>
           <figcaption className="font-medium">
             <div className="text-sky-500 mt-4 ml-270 dark:text-sky-400">
-            Waqas Awan 
+            Tehreem Ashraf
             </div> 
             <div className="text-dark ml-255 dark:text-sky-400">
-              AI Automation, Developer, PAIB,
+             Agentic AI Automation, Developer, PAIB,
+              Tech Enthusiast
             </div>
             </figcaption>
         </blockquote>
