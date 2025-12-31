1c117aa977e2f71cd1d95eaa2c97592831335a94
Tehreem <t1n2b3y4a5h6@gmail.com>
2025-12-31 12:09:26 +0500

    new

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is my about page.
+      You can get any info about me here.
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is my contact page.
+      You can get my help here.
+    </div>
+  )
+}
+
+export default page
